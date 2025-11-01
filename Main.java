aa8701888d4b5ed48de7a662d91e8b8adf2c9d84
lilac6430@gmail.com <lilac6430@gmail.com>
2025-11-01 09:57:23 +0300

    Добавлен базовый генератор случайных чисел

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Генератор случайных чисел");
+        System.out.println("=========================");
+
+        Random random = new Random();
+
+        // Генерация 5 случайных чисел от 1 до 100
+        System.out.println("5 случайных чисел от 1 до 100:");
+        for (int i = 0; i < 5; i++) {
+            int randomNumber = random.nextInt(100) + 1;
+            System.out.println("Число " + (i + 1) + ": " + randomNumber);
+        }
+    }
+}
